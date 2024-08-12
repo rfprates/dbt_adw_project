42827888ec3656f84756ee89fdecce9d8d05bae1
Rodrigo Prates <rfprates.97@gmail.com>
2024-08-12 19:59:03 +0000

    Update dim_salespersons model.

--- models/marts/dim_salespersons.sql
+++ models/marts/dim_salespersons.sql
@@ -1,7 +1,7 @@
 with
     stg_salesperson as (
         select 
-            businessentityid as salesperson_id
+            businessentityid as salespersonid
             , salesytd
             , saleslastyear
         from {{ ref('stg_salesperson') }}
@@ -21,15 +21,14 @@ with
     )
     , joined as (
         select
-            {{ dbt_utils.generate_surrogate_key(['salesperson_id', 'employeebusinessentityid', 'personbusinessentityid']) }} as salesperson_sk
-            , stg_salesperson.salesperson_id
+            stg_salesperson.salespersonid
             , stg_salesperson.salesytd
             , stg_salesperson.saleslastyear
             , stg_employee.loginid
             , stg_person.fullname
             , stg_person.persontype
         from stg_salesperson
-        left join stg_employee on stg_salesperson.salesperson_id = stg_employee.employeebusinessentityid
+        left join stg_employee on stg_salesperson.salespersonid = stg_employee.employeebusinessentityid
         left join stg_person on stg_employee.employeebusinessentityid = stg_person.personbusinessentityid
     )
 select *
