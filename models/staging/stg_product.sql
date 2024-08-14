4d12e98b262ae2a6eafaf51b17fb5c1d91ad9a71
Rodrigo Prates <rfprates.97@gmail.com>
2024-08-14 00:16:54 +0000

    Add "productsubcategoryid" column on stg_product model.

--- models/staging/stg_product.sql
+++ models/staging/stg_product.sql
@@ -5,6 +5,7 @@ with
             , name as product_name
             , makeflag
             , finishedgoodsflag
+            , productsubcategoryid
         from {{ source("sap_adw", "product") }}
     )
 select *
