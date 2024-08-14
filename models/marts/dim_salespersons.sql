a4d1f794ddbb26fbb76ae1e8a946cee37c49f3d1
Rodrigo Prates <rfprates.97@gmail.com>
2024-08-14 23:41:03 +0000

    update dim_salespersons model

--- models/marts/dim_salespersons.sql
+++ models/marts/dim_salespersons.sql
@@ -2,33 +2,72 @@ with
     stg_salesperson as (
         select 
             businessentityid as salespersonid
+            , territoryid
+            , salesquota
+            , bonus 
             , salesytd
             , saleslastyear
+            , commissionpct
         from {{ ref('stg_salesperson') }}
     )
     , stg_employee as (
         select
             businessentityid as employeebusinessentityid
-            , loginid
+            , jobtitle
         from {{ ref('stg_employee') }}
     )
     , stg_person as (
         select
             businessentityid as personbusinessentityid
             , concat(ifnull(firstname,' '),' ',ifnull(middlename,' '),' ',ifnull(lastname,' ')) as fullname
-            , persontype
+            , case
+                when persontype = "SC" then "Store contact"
+                when persontype = "IN" then "Individual customer"
+                when persontype = "SP" then "Sales person"
+                when persontype = "EM" then "Employee"
+                when persontype = "VC" then "Vendor contact"
+                when persontype = "GC" then "General contact"
+            end as person_description
         from {{ ref('stg_person') }}
+    )
+    , stg_salesterritory as (
+        select
+            territoryid
+            , countryregioncode
+            , countryregion
+            , territorygroup
+        from {{ ref('stg_salesterritory')}}
+    )
+    , joined_salesperson_territory as (
+        select
+            stg_salesperson.salespersonid
+            , stg_salesperson.salesquota
+            , stg_salesperson.bonus
+            , stg_salesperson.salesytd
+            , stg_salesperson.saleslastyear
+            , stg_salesperson.commissionpct
+            , stg_salesterritory.countryregioncode
+            , stg_salesterritory.countryregion
+            , stg_salesterritory.territorygroup
+        from stg_salesperson
+        left join stg_salesterritory on stg_salesperson.territoryid = stg_salesterritory.territoryid
     )
     , joined as (
         select
-            stg_salesperson.salespersonid
-            , stg_salesperson.salesytd
-            , stg_salesperson.saleslastyear
-            , stg_employee.loginid
+            joined_salesperson_territory.salespersonid
             , stg_person.fullname
-            , stg_person.persontype
-        from stg_salesperson
-        left join stg_employee on stg_salesperson.salespersonid = stg_employee.employeebusinessentityid
+            , stg_employee.jobtitle
+            , stg_person.person_description
+            , joined_salesperson_territory.salesquota
+            , joined_salesperson_territory.bonus
+            , joined_salesperson_territory.salesytd
+            , joined_salesperson_territory.saleslastyear
+            , joined_salesperson_territory.commissionpct
+            , joined_salesperson_territory.countryregioncode
+            , joined_salesperson_territory.countryregion
+            , joined_salesperson_territory.territorygroup
+        from joined_salesperson_territory
+        left join stg_employee on joined_salesperson_territory.salespersonid = stg_employee.employeebusinessentityid
         left join stg_person on stg_employee.employeebusinessentityid = stg_person.personbusinessentityid
     )
 select *
