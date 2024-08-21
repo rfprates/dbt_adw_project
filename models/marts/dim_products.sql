395c261d8f692a44f272884127d23666f0c85571
Rodrigo Prates <rfprates.97@gmail.com>
2024-08-21 04:13:16 +0000

    Update dim_products model, selecting only the products that are salable.

--- models/marts/dim_products.sql
+++ models/marts/dim_products.sql
@@ -3,8 +3,10 @@ with
         select
             productid
             , product_name
-            , makeflag
-            , finishedgoodsflag
+            , case
+                when finishedgoodsflag = false then 'Product is not a salable item'
+                when finishedgoodsflag = true then 'Product is salable'
+            end as product_salable
             , productsubcategoryid
         from {{ ref('stg_product') }}
     )
@@ -26,13 +28,13 @@ with
             {{ dbt_utils.generate_surrogate_key(['productid', 'product_name']) }} as products_sk
             , stg_product.productid
             , stg_product.product_name
-            , stg_product.makeflag
-            , stg_product.finishedgoodsflag
             , stg_productsubcategory.subcategory_name
             , stg_productcategory.category_name
         from stg_product
         left join stg_productsubcategory on stg_product.productsubcategoryid = stg_productsubcategory.productsubcategoryid
         left join stg_productcategory on stg_productsubcategory.productcategoryid = stg_productcategory.productcategoryid
+        /* selecting only the salable products */
+        where stg_product.product_salable = 'Product is salable'
     )
 select *
 from joined
