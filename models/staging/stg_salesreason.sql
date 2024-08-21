69b8469dc65fb8e7a96fe66a1919d2c4497c0cf7
Rodrigo Prates <rfprates.97@gmail.com>
2024-08-21 04:07:37 +0000

    Update stg_salesreason, excluding "reasontype" column.

--- models/staging/stg_salesreason.sql
+++ models/staging/stg_salesreason.sql
@@ -2,8 +2,7 @@ with
     sales_reason as (
         select
             salesreasonid
-            , name as reason_description
-            , reasontype as reason_type
+            , name as reason_name
         from {{ source("sap_adw", "salesreason") }}
     )
 select *
